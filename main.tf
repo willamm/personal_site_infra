207ac034dd983f84a912e85913fe7be75c89a512
William Murphy <will.murphy1994@gmail.com>
2023-01-23 10:37:13 -0800

    Reduced Cloudfront TTL to 0 for testing

--- main.tf
+++ main.tf
@@ -179,8 +179,8 @@ resource "aws_cloudfront_distribution" "s3_dist" {
 
     viewer_protocol_policy = "allow-all"
     min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
+    default_ttl = 0
+    max_ttl = 0
   }
   
   restrictions {
